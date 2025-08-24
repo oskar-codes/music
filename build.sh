eff5ce77eb01bb2ea9674f761aa5b6aa7b9aef05
Oskar Codes <oskarbertrand@icloud.com>
2025-08-24 16:54:26 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,27 @@ find src -name "*.md" | while read f; do
   rel="${f#src/}"               # relative path under src
   out="out/${rel%.md}.html"     # change .md -> .html
   mkdir -p "$(dirname "$out")"
-  content=$(cmark-gfm "$f")
+  cmark-gfm "$f" > out/.tmp_body.html -e footnotes -e table -e strikethrough -e autolink -e tagfilter -e tasklist --unsafe
+
   # inject into template
-  sed "s|{{CONTENT}}|$content|" template/template.html > "$out"
+  # awk -v r="$content" '{gsub(/{{CONTENT}}/, r)}1' template/template.html > "$out"
+
+  head -n -3 template/template.html > out/.tmp_header.html
+  tail -n 2 template/template.html > out/.tmp_footer.html
+
+  cat out/.tmp_header.html out/.tmp_body.html out/.tmp_footer.html > "$out"
+
+  rm out/.tmp_header.html
+  rm out/.tmp_body.html
+  rm out/.tmp_footer.html
+
 done
 
-# Include CSS
-cp template/github-markdown.css out
+# Except for template/template.html, copy
+# all files of template/ to out/, keeping the same structure
+find template -type f ! -name "template.html" | while read f; do
+  rel="${f#template/}"          # relative path under template
+  out="out/${rel}"              # output path under out
+  mkdir -p "$(dirname "$out")"
+  cp "$f" "$out"
+done
