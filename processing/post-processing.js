7d80f000ef27b76a5bcc16f6c1e117dc36d13458
Oskar Codes <oskarbertrand@icloud.com>
2025-09-04 15:05:23 +0200

    Remove JSDOM and post-processing processes for performance

--- processing/post-processing.js
+++ processing/post-processing.js
@@ -9,16 +9,6 @@
 import { readFile } from 'fs/promises';
 import parseArgs from 'args-parser';
 import { pipe } from './pipeline.js';
-import { JSDOM } from 'jsdom';
-
-function idsOnHeaders(html) {
-  const frag = JSDOM.fragment(`<div>${html}</div>`);
-  const headers = frag.querySelectorAll('h1,h2,h3,h4,h5,h6');
-  for (const header of headers) {
-    header.id = header.textContent.toLowerCase().replace(/ /g, '-');
-  }
-  return frag.firstChild.innerHTML;
-}
 
 function noop(html) {
   return html;
@@ -31,7 +21,7 @@ function noop(html) {
   const result = await readFile(args.file)
   const html = result.toString();
 
-  const pipeline = pipe(idsOnHeaders);
+  const pipeline = pipe(noop);
 
   const processed = pipeline.run(html);
   console.log(processed);
